93b90fe99d88e27ab587d3bbff488024581e2fa6
Giorgos Kourakos <giorgk@gmail.com>
2023-04-21 13:38:22 +0300

    Create readNPSAT_Wells.m

---
+++ Matlab/readNPSAT_Wells.m
@@ -0,0 +1,16 @@
+function wells = readNPSAT_Wells(filename)
+% wells = readWells(filename)
+% Reads the NPSAT well input file. 
+% The output is a table where the rows are:
+% X, Y are the well coordinates
+% Top, Bot are the top and bottom elevation of the well screen
+% Q is the pumping rate
+
+
+fid = fopen(filename,'r');
+Nwells = fscanf(fid, '%d',1);
+temp = fscanf(fid, '%f',Nwells*5);
+fclose(fid);
+wells = reshape(temp, 5, Nwells)';
+
+wells = array2table(wells, 'VariableNames', {'X','Y','Top','Bot', 'Q'});
