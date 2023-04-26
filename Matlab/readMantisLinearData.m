55941539359b647647e57a67b4792db707a5e5f5
Georgios Kourakos <giorgk@gmail.com>
2023-04-26 03:36:52 -0700

    Create readMantisLinearData.m

---
+++ Matlab/readMantisLinearData.m
@@ -0,0 +1,24 @@
+function LD = readMantisLinearData(filename)
+%readMantisLinearData read the data from the linear data format
+C = strsplit(filename,'.');
+if strcmp(C{end},'h5')
+
+else
+    fid = fopen(filename,'r');
+    Ndata = cell2mat(textscan(fid,'%f',1));
+    for ii = 1:Ndata
+        tmp = textscan(fid,'%s',1);
+        LD.Names{ii,1} = tmp{1};
+    end
+    fclose(fid);
+    frmt = '%f';
+    if Ndata > 1
+        frmt = [frmt repmat(' %f',1,Ndata-1)];
+    end
+    fid = fopen(filename,'r');
+    C = textscan(fid, frmt,'HeaderLines',Ndata+1);
+    fclose(fid);
+    LD.Data = cell2mat(C);
+end
+
+end
